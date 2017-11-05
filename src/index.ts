6e72282f8314367d34fd0c17e719fa0910752d27
john doe <hayashikotoba@gmail.com>
2017-11-05 01:43:30 -0700

    Add GDAX api types

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,76 @@
-/// <reference path="node.d.ts"/>
-import * as GDAX from 'gdax';
+import {AuthenticatedClient, WebsocketClient} from 'gdax';
 
 import {passPhrase, apiKey, secret} from './credentials';
 import ACCOUNTS from './accounts';
 
-const authenticatedClient = new GDAX.AuthenticatedClient(
+const authenticatedClient = new AuthenticatedClient(
     apiKey, secret, passPhrase, 'https://api.gdax.com');
 
 const callback = (error, response, data) => {
-    if (error)
-        return console.dir(error);
-    return console.dir(data);
+  if (error)
+    return console.dir(error);
+  return console.dir(data);
 };
+// authenticatedClient.getAccounts(callback);
 // authenticatedClient.getAccount(ACCOUNTS.ETH, callback);
 
-const ETH_USD = 'ETH-USD';
+const websocketConfig = {
+    "type": "subscribe",
+    "product_ids": [
+        "ETH-USD",
+        "ETH-EUR"
+    ],
+    "channels": [
+        "level2",
+        "heartbeat",
+        {
+            "name": "ticker",
+            "product_ids": [
+                "ETH-USD",
+            ]
+        },
+    ]
+};
 
-const websocket = new GDAX.WebsocketClient([ETH_USD]);
+// const orderbookSync = new OrderbookSync(['BTC-USD', 'ETH-USD']);
+// console.log(orderbookSync.books['ETH-USD'].state());
+const websocket = new WebsocketClient(['BTC-USD']);
+
 websocket.on('message', (data: any) => {
-  // Get filled orders.
-  if (!(data.type === 'done' && data.reason === 'filled')) {
-    return;
-  }
+  console.log(data);
+  // if(data.type == 'ticker') {
+  //   return console.dir(data);
+  // }
 
-  console.dir(data)
+  // if (!(data.type === 'done')) {
+  //   return;
+  // }
+  
+  // const {buyOrder, sellOrder} = Strategy.decide(data as Signal);
+  // // const buyId: OrderId = buyOrder ? authenticatedClient.sell(buyOrder, () => {}) : null;
+  // // const sellId: OrderId = sellOrder ?  authenticatedClient.buy(sellOrder, () => {}) : null;
+
+  // return console.dir(data)
 });
+
+// interface Decision {
+//   // TODO: These should be generalized to a collection.
+//   buyOrder: Order | null;
+//   sellOrder: Order | null;
+// }
+
+// type Signal = any;
+// type Position = any;
+
+// module Strategy {
+//   export function decide(signal: Signal = null, position: Position = null): Decision {
+//     const buyOrder = null;
+//     const sellOrder = null;
+//     return {
+//       buyOrder,
+//       sellOrder
+//     };
+//   }
+// }
+
+
